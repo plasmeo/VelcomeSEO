70374b5b3406b7519992a1b17cdc1c52392f00cd
Thomas Miart <thomas.miart@gmail.com>
2022-11-01 09:41:47 +0100

    first commit

---
+++ theme/footer.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="logo-phone">
+        <a class="footer-logo" href="/">
+            <?php
+            $custom_logo_id = get_theme_mod( 'custom_logo' );
+            $image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+            ?>
+            <img src="<?php echo $image[0]; ?>" width="" height="75" alt="Logo">
+        </a>
+            <div class="phone"> 
+            <a href="tel:+33950236253">
+                <span class="cons dashicons-phone"></span>
+                Téléphone: 09 50 23 62 53
+</a>
+            </div>
+            </div>
+    <nav class="footer-menu">
+    <?php 
+        wp_nav_menu( array(
+            'theme_location' => 'footer'
+        ) );
+    ?>
+    </nav>
+</footer>
+
+<?php wp_footer();?>
